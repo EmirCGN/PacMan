f5d5dc6d29c7e4fd4d08d3cc4c2220f4b0a730c5
Emir <muhammedem.k@gso.schule.koeln>
2023-05-25 00:59:49 +0200

    Import hinzugefügt

--- PacMan/Program.cs
+++ PacMan/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace PacMan
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PacMan
 {
     class Program
     {
