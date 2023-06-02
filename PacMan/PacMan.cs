bea74ff92896aa41ecbd4f8e4798b24288c87495
Emir <muhammedem.k@gso.schule.koeln>
2023-06-02 19:17:43 +0200

    Bug fixes

--- PacMan/PacMan.cs
+++ PacMan/PacMan.cs
@@ -11,5 +11,45 @@ namespace PacMan
         public PacMan(int x, int y) : base(x, y, 'P')
         {
         }
+
+        public void MoveUp(char[,] gameBoard)
+        {
+            if (Y > 0 && gameBoard[Y - 1, X] != '#')
+            {
+                Y--;
+                if (gameBoard[Y, X] == '.')
+                    gameBoard[Y, X] = ' ';
+            }
+        }
+
+        public void MoveDown(char[,] gameBoard)
+        {
+            if (Y < gameBoard.GetLength(0) - 1 && gameBoard[Y + 1, X] != '#')
+            {
+                Y++;
+                if (gameBoard[Y, X] == '.')
+                    gameBoard[Y, X] = ' ';
+            }
+        }
+
+        public void MoveLeft(char[,] gameBoard)
+        {
+            if (X > 0 && gameBoard[Y, X - 1] != '#')
+            {
+                X--;
+                if (gameBoard[Y, X] == '.')
+                    gameBoard[Y, X] = ' ';
+            }
+        }
+
+        public void MoveRight(char[,] gameBoard)
+        {
+            if (X < gameBoard.GetLength(1) - 1 && gameBoard[Y, X + 1] != '#')
+            {
+                X++;
+                if (gameBoard[Y, X] == '.')
+                    gameBoard[Y, X] = ' ';
+            }
+        }
     }
 }
